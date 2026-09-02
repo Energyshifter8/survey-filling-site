a216299dc828532eb11b56e9c2d3fb665f1fd446
Energyshifter8 <temuulenbold1@gmail.com>
2026-09-02 10:17:17 +0800

    start from scratch

--- lib/survey-pages.ts
+++
@@ -1,8 +0,0 @@
-import type { PageDTO, PageType, SurveyPublicDto } from "@/lib/api/types";
-
-/** Finds the first page of a given type across all sections (START/TEST/END) of a survey. */
-export function findSurveyPage(survey: SurveyPublicDto, pageType: PageType): PageDTO | undefined {
-  return Object.values(survey.pages ?? {})
-    .flat()
-    .find((page) => page?.pageType === pageType);
-}
